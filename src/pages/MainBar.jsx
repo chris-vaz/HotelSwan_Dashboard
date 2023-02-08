c41b89c10040731416242deb6458f9a6a30ebef7
Chris Vaz <76993434+chris-vaz@users.noreply.github.com>
2023-02-08 17:12:30 +0300

    Completed Side-Navigation Bar

--- src/pages/MainBar.jsx
+++ src/pages/MainBar.jsx
@@ -3,9 +3,27 @@ import mbstyle from '../styles/mbstyle.css'
 
 const MainBar = () => {
     return (
-        <div class="container">
-            MainBar
+        <>
+        <div class="main-container">
+            <div class="search-section">
+                <ion-icon name="search-outline"></ion-icon>
+                <p>Search</p>
+            </div>
+            
+            <h1 class="welcome"><span class="grey-shade">Welcome, </span>William</h1>
+            <p>You have 9 free rooms at Saint Denis</p>
+
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+            <div>
+
+            </div>
         </div>
+        </>
     )
 }
 
--- src/pages/SideBar.jsx
+++ src/pages/SideBar.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import sbstyle from '../styles/sbstyle.css'
+import hslogo from '../assets/hs-logo.png'
 
 const SideBar = () => {
     return (
+        <>
         <div class="side-container">
-            SideBar
+
+                <div class="center-div">
+                <img src={hslogo} alt="Hotel Swan Logo" class="hslogo" />
+                </div>
+
+                <div class="sidebar-options">
+                    <p><span><ion-icon name="bar-chart-outline"></ion-icon></span>Overview</p>
+                    <p><span><ion-icon name="bar-chart-outline"></ion-icon></span>Calendar</p>
+                    <p><span><ion-icon name="bar-chart-outline"></ion-icon></span>Booking</p>
+                    <p><span><ion-icon name="bar-chart-outline"></ion-icon></span>Earnings</p>
+                    <p><span><ion-icon name="bar-chart-outline"></ion-icon></span>Stuff</p>
+                </div>
+
+                <div class="settings">
+                    <div class="setting-icon"><ion-icon name="bag-add-outline"></ion-icon></div>
+                    <div class="setting-text"><p>Settings</p></div>
+                </div>
+
         </div>
+        </>
     )
 }
 

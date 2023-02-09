95db65e806f7d224ce27405094befcb36ac8a250
Chris Vaz <76993434+chris-vaz@users.noreply.github.com>
2023-02-09 14:44:42 +0300

    Completed structure of Action Bar

--- src/pages/ActionBar.jsx
+++ src/pages/ActionBar.jsx
@@ -1,32 +1,72 @@
 import React from 'react'
 import abstyle from '../styles/abstyle.css'
+import avatar1 from '../assets/avatar-1.png'
+import avatar2 from '../assets/avatar-2.png'
+import wallpaper from '../assets/wallpaper.png'
 
 const ActionBar = () => {
     return (
         <>
             <div class="action-container">
                 <div>
-
-                    <div>Notification</div>
-                    <div>
-                        <div>Profile pic</div>
-                        <div>William Dawson</div>
+                    <div class="profile-section">
+                        <div class="notification-icon"><ion-icon name="notifications-outline"></ion-icon></div>
+                        <div id="align-1">
+                            <div class="avatar-name">William Dawson</div>
+                            <div><img src={avatar1} alt="Avatar-1 PFP" class="avatar-1" /></div>
+                        </div>
                     </div>
 
-                    <div>image</div>
+                    <div id="center-div"><img src={wallpaper} alt="Wallpaper Display" class="wallpaper" /></div>
 
-                    <div>
-                        <div>Booking List</div>
-                        <div>Options</div>
+                    <div class="list-heading-section">
+                        <div class="bl-text">Booking List</div>
+                        <div><ion-icon name="ellipsis-horizontal-outline"></ion-icon></div>
                     </div>
 
-                    <div>Wade Warren</div>
-                    <div>Jacob Jones</div>
-                    <div>Kathryn Murphy</div>
+                    <div id="flex-items-row">
+                        <img src={avatar2} alt="Avatar-2-PFP" class="avatar-2"></img>
+                        <div>
+                            <p>Wade Warren</p>
+                            <p>#SGL <ion-icon name="ellipse"></ion-icon> 0 guests</p>
+                        </div>
+                        <div>
+                            <p>01.12-01.14 </p>
+                            <button>Paid</button>
+                        </div>
 
-                    <div>View All</div>
+                    </div>
+                    <div id="flex-items-row">
+                        <img src={avatar2} alt="Avatar-2-PFP" class="avatar-2"></img>
+                        <div>
+                            <p>Jacob Jones</p>
+                            <p>#SGL <ion-icon name="ellipse"></ion-icon> 0 guests</p>
+                        </div>
+                        <div>
+                            <p>01.12-01.14 </p>
+                            <button>Paid</button>
+                        </div>
 
-                    <div>Unlock new features</div>
+                    </div>
+                    <div id="flex-items-row">
+                        <img src={avatar2} alt="Avatar-2-PFP" class="avatar-2"></img>
+                        <div>
+                            <p>Kathryn Murphy</p>
+                            <p>#SGL <ion-icon name="ellipse"></ion-icon> 0 guests</p>
+                        </div>
+                        <div>
+                            <p>01.12-01.14 </p>
+                            <button>Paid</button>
+                        </div>
+
+                    </div>
+
+                    <div class="view-all">View All</div>
+
+                    <div class="unlock-section">
+                        <p>Unlock new features</p>
+                        <button>Get Pro Now!</button>
+                    </div>
                 </div>
             </div>
         </>
